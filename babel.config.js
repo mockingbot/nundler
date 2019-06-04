579b7f7d96fbc9ac0b033e9f0189cbf8b3efeaa2
dr-js <dr@dr.run>
2019-06-04 11:20:18 +0800

    nundler@0.0.3
    notable change:
    - use explicit webpack/babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
-const { getBabelConfig } = require('dr-dev/library/babel')
+const BABEL_ENV = process.env.BABEL_ENV || ''
+const isDev = BABEL_ENV.includes('dev')
+const isModule = BABEL_ENV.includes('module')
 
-module.exports = getBabelConfig()
+module.exports = {
+  presets: [
+    [ '@babel/env', { targets: { node: '10' }, modules: isModule ? false : 'commonjs' } ]
+  ],
+  plugins: [
+    [ 'minify-replace', { replacements: [ { identifierName: '__DEV__', replacement: { type: 'booleanLiteral', value: isDev } } ] } ],
+    [ 'module-resolver', {
+      root: [ './' ],
+      alias: isModule ? undefined : { '^dr-([\\w-]+)/module/(.+)': 'dr-\\1/library/\\2' }
+    } ]
+  ],
+  comments: false
+}
--- script/webpack-bin.js
+++ script/webpack-bin.js
@@ -1,4 +1,5 @@
 import { resolve } from 'path'
+import { DefinePlugin, BannerPlugin } from 'webpack'
 
 import { runMain } from 'dr-dev/module/main'
 import { compileWithWebpack, commonFlag } from 'dr-dev/module/webpack'
@@ -9,19 +10,34 @@ const fromRoot = (...args) => resolve(PATH_ROOT, ...args)
 const fromOutput = (...args) => resolve(PATH_OUTPUT, ...args)
 
 runMain(async (logger) => {
-  const { mode, isWatch, profileOutput, assetMapOutput, getCommonWebpackConfig } = await commonFlag({
+  const { mode, isWatch, isProduction, profileOutput, assetMapOutput } = await commonFlag({
     profileOutput: fromRoot('.temp-gitignore/profile-stat-bin.json'),
     logger
   })
 
-  const config = getCommonWebpackConfig({
-    isNodeEnv: true,
-    isNodeBin: true,
+  const babelOption = {
+    configFile: false,
+    babelrc: false,
+    cacheDirectory: isProduction,
+    presets: [ [ '@babel/env', { targets: { node: '10' }, modules: false } ] ]
+  }
+
+  const config = {
+    mode,
+    bail: isProduction,
+    target: 'node', // support node main modules like 'fs'
+    node: false, // do not polyfill fake node environment
     output: { path: fromOutput('bin'), filename: '[name].js' },
     entry: { index: 'source-bin/index' },
     resolve: { alias: { 'source-bin': fromRoot('source-bin') } },
-    externals: { 'source': `require('../library')` }
-  })
+    externals: { 'source': `require('../library')` },
+    module: { rules: [ { test: /\.js$/, use: [ { loader: 'babel-loader', options: babelOption } ] } ] },
+    plugins: [
+      new DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(mode), '__DEV__': !isProduction }),
+      new BannerPlugin({ banner: `#!/usr/bin/env node`, raw: true })
+    ],
+    optimization: { minimize: false }
+  }
 
   logger.padLog(`compile with webpack mode: ${mode}, isWatch: ${Boolean(isWatch)}`)
   await compileWithWebpack({ config, isWatch, profileOutput, assetMapOutput, logger })
